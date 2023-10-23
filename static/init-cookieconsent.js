55d0d09df8dd2e033e434133e1871f05af184cf1
Mo. Mirmousavi <mirmousavi.m@gmail.com>
2023-10-23 23:31:38 +0200

    add custom theme

---
+++ static/init-cookieconsent.js
@@ -0,0 +1,13 @@
+// https://cookieconsent.insites.com/download/#
+window.addEventListener('load', function () {
+    window.cookieconsent.initialise({
+        'palette': {
+            'popup': {
+                'background': '#2D3642'
+            },
+            'button': {
+                'background': '#fff'
+            }
+        }
+    })
+})
